310f26db829838f4d0f9ff9f7b475a8799a7dd3c
Łukasz Pająk <lukas.pajak27@gmail.com>
2019-03-29 00:19:36 +0100

    Add RSS Source object. Change rsslist file structure

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import feedparser
 
+class RSSSource:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def add_news_list(self, newsList):
+        self.newsList = newsList;
+
+    def __str__(self):
+        return "RSSSource object:\n Source name is %s Source URL is %s" % (self.name, self.url)
+
 if __name__ == '__main__':
-    with open("rsslist", "r") as ins:
-        rssArray = []
-        for line in ins:
-            rssArray.append(line)
+    rssArray = []
+    with open("rsslist", "r") as file:
+        while True:
+            name = file.readline()
+            url = file.readline()
+            if not url: break  # EOF
+            rssArray.append(RSSSource(name, url))
 
-    NewsFeed = feedparser.parse(rssArray[0])
-    entry = NewsFeed.entries[1]
-
-    print(entry.title)
-
+    for a in rssArray:
+        newsFeed = feedparser.parse(a.url)
+        a.add_news_list(newsFeed)
+#    for rss in rssArray:
+#        rss.add_news_list(feedparser.parse(rss.url)
+#    for rss in rssArray:
+#        print(rss)
+#        print(rss.newsList.size)
+#
+#    print(entry.title)
+    for a in rssArray:
+        print(a)
+        print(a.newsList)
+        print("--------------------------------------------")
