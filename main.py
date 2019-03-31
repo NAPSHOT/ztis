ca35514eabaf390260fec95f583582c253efabc2
Łukasz Pająk <lukas.pajak27@gmail.com>
2019-03-31 16:24:21 +0200

    Refactor code. Add source column to database. Add example response file.

--- main.py
+++ main.py
@@ -1,52 +1,75 @@
 import feedparser
 import sqlite3
+rssArray = []
 
-conn = sqlite3.connect('ztis.db')
-cur = conn.cursor()
+def create_table(cur):
+    try:
+        cur.execute("""CREATE TABLE ztis
+                 (source, title, summary, publication_date, author)""")
+    except:
+        pass
 
+def drop_table(cur):
+    try:
+        cur.execute("DROP TABLE ztis")
+    except:
+        pass
 
-class RSSSource:
-
-    def __init__(self, name, url):
-        self.name = name
-        self.url = url
-
-    def add_news_list(self, newsList):
-        self.newsList = newsList;
-
-    def __str__(self):
-        return "RSSSource object:\n Source name is %s Source URL is %s" % (self.name, self.url)
-
-if __name__ == '__main__':
-    rssArray = []
+def read_rsslist():
     with open("rsslist", "r") as file:
         while True:
             name = file.readline()
             url = file.readline()
             if not url: break  # EOF
             rssArray.append(RSSSource(name, url))
+    print("Read %d sources" % (len(rssArray)))
 
+def download_data():
     for a in rssArray:
+        print("Downloading feed from " + a.name)
         newsFeed = feedparser.parse(a.url)
         a.add_news_list(newsFeed)
-        for item in newsFeed.entries:
-            try:
-                title2 = item['title']
-            except KeyError:
-                title2 = 'default'
-            try:
-                publication_date2 = item['published']
-            except KeyError:
-                publication_date2 = 'default'
-            try:
-                author2 = item['author']
-            except KeyError:
-                author2 = 'default'
-            try:
-                summary2 = item['summary']
-            except KeyError:
-                summary2 = 'default'
 
-
-            cur.execute("INSERT INTO ztis VALUES (?, ?, ?, ?)", (title2, summary2, publication_date2, author2))
+def save_data(conn, cur):
+    for a in rssArray:
+        for item in a.newsList.entries:
+            source = a.name
+            try:
+                title = item['title']
+            except KeyError:
+                title = 'default'
+            try:
+                publication_date = item['published']
+            except KeyError:
+                publication_date = 'default'
+            try:
+                author = item['author']
+            except KeyError:
+                author = 'default'
+            try:
+                summary = item['summary']
+            except KeyError:
+                summary = 'default'
+            cur.execute("INSERT INTO ztis VALUES (?, ?, ?, ?, ?)", (source, title, summary, publication_date, author))
             conn.commit()
+
+class RSSSource:
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def add_news_list(self, newsList):
+        self.newsList = newsList
+
+    def __str__(self):
+        return "RSSSource object:\n Source name is %s Source URL is %s" % (self.name, self.url)
+
+if __name__ == '__main__':
+    conn = sqlite3.connect('ztis.db')
+    cur = conn.cursor()
+    read_rsslist()
+    download_data()
+#    drop_table(cur)
+#    create_table(cur)
+    save_data(conn, cur)
+    conn.close()
